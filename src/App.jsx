c6b13bac026332b61b5449f9ffa5f4eb1a161d86
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-08-17 00:16:14 +0530

    app main comp

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
+
 import "./App.css";
 import Header from './components/Header';
 import RecipeListContainer from "./components/Recipe"
 
 function App() {
+
+  const [recipeList, setRecipeList] = useState();
+
   return(
     <>
-      <Header></Header>
-      <RecipeListContainer></RecipeListContainer>
+      <Header recipeList={recipeList} setRecipeList={setRecipeList}></Header>
+      <RecipeListContainer recipeList={recipeList} setRecipeList={setRecipeList}></RecipeListContainer>
     </>
   )
 }
