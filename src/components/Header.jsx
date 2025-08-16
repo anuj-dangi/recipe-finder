199037e93396fd98488ec13c34dabd5213879297
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-08-17 00:16:07 +0530

    Header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,58 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Axios from "axios";
 
 import "../css/header.css";
 
-const Header = () => {
+const Header = ({ recipeList, setRecipeList }) => {
+  const [timeoutId, setTimeoutId] = useState();
+
+  const fetchRecipe = async (searchString) => {
+    try {
+      if (!searchString || searchString.trim() === "") {
+        const response = await Axios.get("https://www.themealdb.com/api/json/v1/1/random.php");
+        setRecipeList(response.data.meals);
+      } else {
+        const response = await Axios.get(
+          `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchString}`
+        );
+        if (response.data.meals) {
+          setRecipeList(response.data.meals);
+        } else {
+          const randomRes = await Axios.get("https://www.themealdb.com/api/json/v1/1/random.php");
+          setRecipeList(randomRes.data.meals);
+        }
+      }
+    } catch (error) {
+      console.log("Error fetching recipe, Error: ", error);
+    }
+  };
+
+  const onTextChange = (event) => {
+    clearTimeout(timeoutId);
+    const timeout = setTimeout(() => fetchRecipe(event.target.value), 500);
+    setTimeoutId(timeout);
+  };
+
+  useEffect(() => {
+    fetchRecipe("");
+  }, []);
+
   return (
-    <div className='header'> 
-      <div className='appName'>
-          <img className='appIcon' src="/hamburger.svg"/>
-          Recipe Finder
+    <div className="header">
+      <div className="appName">
+        <img className="appIcon" src="/hamburger.svg" alt="logo" />
+        Recipe Finder
       </div>
-        <div className="searchComponent">
-          <img src="/search-icon.svg"/>
-          <input className='searchInput' placeholder='Seach Recipe'/>
-        </div>
+      <div className="searchComponent">
+        <img src="/search-icon.svg" alt="search" />
+        <input
+          className="searchInput"
+          placeholder="Search Recipe"
+          onChange={onTextChange}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Header;
