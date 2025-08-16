e429c61432533d2efd6c6f4bce65022ba7a3e82f
Anuj Dangi <23mcce16@uohyd.ac.in>
2025-08-16 22:41:53 +0530

    Recipe Component

---
+++ src/components/Recipe.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import "../css/recipe.css";
+
+const RecipeListContainer = () => {
+  return (
+    <div className='recipeListContainer'>
+        <div className='recipeContainer'>
+            <img className='coverImage' src="hamburger.svg"/>
+            <span className='recipeName'>Matar Panner</span>
+            <span className='ingredientsText'>Ingredients</span>
+            <span className='seeMoreText'>See Complete Recipe</span>
+        </div>
+        <div className='recipeContainer'>
+            <img className='coverImage' src="hamburger.svg"/>
+            <span className='recipeName'>Matar Panner</span>
+            <span className='ingredientsText'>Ingredients</span>
+            <span className='seeMoreText'>See Complete Recipe</span>
+        </div>
+        <div className='recipeContainer'>
+            <img className='coverImage' src="hamburger.svg"/>
+            <span className='recipeName'>Matar Panner</span>
+            <span className='ingredientsText'>Ingredients</span>
+            <span className='seeMoreText'>See Complete Recipe</span>
+        </div>
+        <div className='recipeContainer'>
+            <img className='coverImage' src="hamburger.svg"/>
+            <span className='recipeName'>Matar Panner</span>
+            <span className='ingredientsText'>Ingredients</span>
+            <span className='seeMoreText'>See Complete Recipe</span>
+        </div>
+        <div className='recipeContainer'>
+            <img className='coverImage' src="hamburger.svg"/>
+            <span className='recipeName'>Matar Panner</span>
+            <span className='ingredientsText'>Ingredients</span>
+            <span className='seeMoreText'>See Complete Recipe</span>
+        </div>
+    </div>
+  )
+}
+
+export default RecipeListContainer
